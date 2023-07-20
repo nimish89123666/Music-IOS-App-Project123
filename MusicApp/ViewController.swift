d46360e843da0a96cad190f887d2db0ae186dd9f
Nimish Mothghare <nimishm90@gmail.com>
2023-07-21 02:49:38 +0530

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Nimish Mothgare on 21/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
